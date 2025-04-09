a85e9188a1690ce761b4916181c0ad6dbfa2cc38
Patrick Brochu <patrickbrochu70@gmail.com>
2025-04-09 15:21:13 -0400

    Title and hamburger header completed with dropdown menu
    Completed the slideshow behavior and visual
    
    TODO:Add a progress bar bellow the slideshow to
    keep track of the progress with empty and filled dot

---
+++ script.js
@@ -0,0 +1,93 @@
+console.log("hello world");
+
+const hamburgerButton = {
+  mouseOnListener: (function () {
+    document
+      .querySelector(".hamburger-button")
+      .addEventListener("mouseover", function () {
+        document.querySelector(".dropdown-div").classList.remove("invisible");
+      });
+  })(),
+  mouseOffListener: (function () {
+    document
+      .querySelector(".dropdown-div")
+      .addEventListener("mouseleave", function () {
+        document.querySelector(".dropdown-div").classList.add("invisible");
+      });
+  })(),
+};
+
+const carousel = {
+  focusedImageDiv: document.querySelector(".carousel-focus"),
+  leftImageDiv: document.querySelector(".carousel-left-wrapper"),
+  rightImageDiv: document.querySelector(".carousel-right-wrapper"),
+
+  rightArrowClick: (function () {
+    document
+      .querySelector(".arrow-right")
+      .addEventListener("click", function () {
+        carousel.slideMoveLeft();
+      });
+  })(),
+  leftArrowClick: (function () {
+    document
+      .querySelector(".arrow-left")
+      .addEventListener("click", function () {
+        carousel.slideMoveRight();
+      });
+  })(),
+  slideMoveRight: function () {
+    let oldLeftImage = carousel.leftImageDiv //old the left img photo before it changes
+      .querySelector(".carousel-images")
+      .getAttribute("src");
+
+    carousel.leftImageDiv
+      .querySelector(".carousel-images")
+      .setAttribute(
+        "src",
+        carousel.focusedImageDiv
+          .querySelector(".carousel-images")
+          .getAttribute("src"),
+      );
+
+    carousel.focusedImageDiv
+      .querySelector(".carousel-images")
+      .setAttribute(
+        "src",
+        carousel.rightImageDiv
+          .querySelector(".carousel-images")
+          .getAttribute("src"),
+      );
+
+    carousel.rightImageDiv
+      .querySelector(".carousel-images")
+      .setAttribute("src", oldLeftImage);
+  },
+  slideMoveLeft: function () {
+    let oldLeftImage = carousel.leftImageDiv //Hold the right img photo before it changes
+      .querySelector(".carousel-images")
+      .getAttribute("src");
+
+    carousel.leftImageDiv
+      .querySelector(".carousel-images")
+      .setAttribute(
+        "src",
+        carousel.rightImageDiv
+          .querySelector(".carousel-images")
+          .getAttribute("src"),
+      );
+
+    carousel.rightImageDiv
+      .querySelector(".carousel-images")
+      .setAttribute(
+        "src",
+        carousel.focusedImageDiv
+          .querySelector(".carousel-images")
+          .getAttribute("src"),
+      );
+
+    carousel.focusedImageDiv
+      .querySelector(".carousel-images")
+      .setAttribute("src", oldLeftImage);
+  },
+};
